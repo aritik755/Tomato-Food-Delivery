8b3c05b4f036ebf5ca8ffb2e33a39b641c33889a
aritik755 <aritik755@gmail.com>
2026-08-20 19:03:23 +0530

    Changes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // Placing User Order From Frontend
 const placeOrder = async (req,res) => {
 
-  const frontend_url = "http://localhost:5174"
+  const frontend_url = "https://tomato-food-delivery-frontend-sgow.onrender.com/"
 
   try {
     const newOrder = new orderModel({
