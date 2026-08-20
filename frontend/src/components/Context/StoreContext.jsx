b61c38eee3bdc2ad3ae114ab8a7b6009274fa4bc
aritik755 <aritik755@gmail.com>
2026-08-20 15:37:50 +0530

    Frontend of the Project

---
+++ frontend/src/components/Context/StoreContext.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
+  const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
+  const [food_list, setFoodList] = useState([])
+
+  const addToCart = async (itemId) =>{
+    if(!cartItems[itemId]){
+      setCartItems((prev)=>({...prev,[itemId]:1}))
+    }
+    else{
+      setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+    }
+    if(token){
+      await axios.post(url+"/api/cart/add",{itemId}, {headers:{token}})
+    }
+  }
+
+  const removeFromCart = async (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    if(token){
+      await axios.post(url+"/api/cart/remove",{itemId}, {headers:{token}})
+    } 
+  }
+
+  const getTotalCartAmount = () =>{
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item] > 0){
+        let itemInfo = food_list.find((product)=>product._id === item)
+        totalAmount += itemInfo.price*cartItems[item]; 
+      }
+    }
+    return totalAmount;
+  }
+
+  const fetchFoodList = async(req,res) => {
+    const response = await axios.get(url+"/api/food/list");
+    setFoodList(response.data.data)
+  }
+
+  const loadCartData = async (token) => {
+    const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
+    setCartItems(response.data.cartData)
+  }
+
+  useEffect(()=>{
+   
+    async function loadData() {
+      await fetchFoodList();
+
+      const savedToken = localStorage.getItem("token");
+      if(savedToken){
+        setToken(savedToken);
+        await loadCartData(savedToken);
+      }
+    }
+    loadData();
+  },[])
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart, 
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
+
+  }
+
+  useEffect(()=>{
+    console.log(cartItems);
+  },[cartItems])
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  )
+}
+
+export default StoreContextProvider
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <div className="header-contents">
+        <h2>Order you favourite food here</h2>
+        <p>Choose froma diverse menu featuring a detectable array of dishes crafted with the finest ingredients and elevate your dining experience, one delicious meal at a time. </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
